4cff5075a398107d0d36fb52b86d8fae958c07d9
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2021-12-14 23:30:59 +0700

    Solusi untuk problem 4, membaca input key Arrow dari user, kemudian melakukan AddForce pada Sphere sesuai dengan input

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        int _x = 0;
+        int _y = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            _y += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            _y -= 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            _x -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            _x += 1;
+        }
+        SphereController.Instance.Move(_x, _y);
+    }
+}
--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -7,6 +7,24 @@ public class SphereController : MonoBehaviour
     private Rigidbody2D rigidBody2D;
     public float xMaxForce = 10;
     public float yMaxForce = 10;
+    public float yMoveForce = 10;
+    public float xMoveForce = 10;
+
+    int moveX = 0;
+    int moveY = 0;
+
+    public float MoveEventInterval = 0.1f;
+    private float _moveEventCounter;
+
+    private static SphereController _instance = null;
+    public static SphereController Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<SphereController>();
+            }
+            return _instance;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +36,18 @@ public class SphereController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float deltaTime = Time.unscaledDeltaTime;
+        _moveEventCounter -= deltaTime;
+
+        if (_moveEventCounter < 0f) {
+            _moveEventCounter = MoveEventInterval;
+
+            if ((moveX != 0) || (moveY != 0)) {
+                rigidBody2D.AddForce(new Vector2(moveX * xMoveForce, moveY * yMoveForce));
+                moveX = 0;
+                moveY = 0;
+            }
+        }
     }
 
     void PushSphere() {
@@ -36,6 +65,11 @@ public class SphereController : MonoBehaviour
         float penguatanGaya = gayaMaksimum / gayaSekarang;
 
         rigidBody2D.AddForce(new Vector2(xRandomForce * penguatanGaya, yRandomForce * penguatanGaya));
+    }
+
+    public void Move(int _x, int _y) {
+        moveX = _x;
+        moveY = _y;
     }
 
     void ResetBall() {
