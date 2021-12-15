0a88d7d475f2fd03403a3f02a97aa6999394c1e0
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2021-12-15 13:39:41 +0700

    Menambahkan Scene Main Menu

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void MainMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+}
